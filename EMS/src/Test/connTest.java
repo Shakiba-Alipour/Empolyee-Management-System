7ecf0567c720669c9669e69df1cd21a3e9252ed7
Shakiba <alipour.sh79@gmail.com>
2023-02-09 18:35:26 +0330

    connTest is completed

--- EMS/src/Test/connTest.java
+++ EMS/src/Test/connTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+import Employee.conn;
+import org.junit.Test;
 
-class connTest {
+import java.sql.Connection;
+import java.sql.Statement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class connTest {
+
+    @Test
+    public void connTest(){
+        conn conn=new conn();
+        Connection connection=conn.c;
+        Statement statement=conn.st;
+
+        assertEquals("com.mysql.cj.jdbc.Driver", connection.getClass().getName());
+        assertNotNull(statement);
+    }
 }
