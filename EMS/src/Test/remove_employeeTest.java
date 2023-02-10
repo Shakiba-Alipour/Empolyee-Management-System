de2133fb2c6c3b1f4181814591f83dae41daa876
Shakiba <alipour.sh79@gmail.com>
2023-02-10 10:47:39 +0330

    labels' tests are added to remove_employeeTest

--- EMS/src/Test/remove_employeeTest.java
+++ EMS/src/Test/remove_employeeTest.java
@@ -2,6 +2,7 @@ import Employee.remove_employee;
 import org.junit.After;
 import org.junit.Before;
 
+import javax.swing.*;
 import java.awt.*;
 
 import static org.junit.Assert.assertNotNull;
@@ -46,7 +47,121 @@ class remove_employeeTest {
                 () -> assertEquals(true, remove_employee.frame.isVisible())
         );
 
+        assertAll("Label 5",
+                () -> assertEquals(0, remove_employee.l5.getX()),
+                () -> assertEquals(0, remove_employee.l5.getY()),
+                () -> assertEquals(500, remove_employee.l5.getWidth()),
+                () -> assertEquals(500, remove_employee.l5.getHeight()),
+                () -> assertEquals(null, remove_employee.l5.getLayout())
+        );
+        ImageIcon i1 = (ImageIcon) remove_employee.l5.getIcon();
+        assertNotNull(i1);
 
+        assertAll("Employee Id Label",
+                () -> assertEquals("Employee ID", remove_employee.l1.getText()),
+                () -> assertEquals(Color.white, remove_employee.l1.getForeground()),
+                () -> assertEquals(50, remove_employee.l1.getX()),
+                () -> assertEquals(50, remove_employee.l1.getY()),
+                () -> assertEquals(250, remove_employee.l1.getWidth()),
+                () -> assertEquals(30, remove_employee.l1.getHeight()),
+                () -> assertEquals("serif", remove_employee.l1.getFont().getName()),
+                () -> assertEquals(25, remove_employee.l1.getFont().getSize()),
+                () -> assertEquals(true, remove_employee.l1.getFont().isBold())
+        );
+
+        assertAll("Text Field",
+                () -> assertEquals(250, remove_employee.t.getX()),
+                () -> assertEquals(50, remove_employee.t.getY()),
+                () -> assertEquals(150, remove_employee.t.getWidth()),
+                () -> assertEquals(30, remove_employee.t.getHeight())
+        );
+
+        assertAll("Search Button",
+                () -> assertEquals("Search", remove_employee.b.getText()),
+                () -> assertEquals(200, remove_employee.b.getX()),
+                () -> assertEquals(100, remove_employee.b.getY()),
+                () -> assertEquals(100, remove_employee.b.getWidth()),
+                () -> assertEquals(30, remove_employee.b.getHeight()),
+                () -> assertEquals(true, remove_employee.b.getActionListeners())
+        );
+
+        assertAll("back Button",
+                () -> assertEquals("back", remove_employee.b3.getText()),
+                () -> assertEquals(360, remove_employee.b3.getX()),
+                () -> assertEquals(100, remove_employee.b3.getY()),
+                () -> assertEquals(100, remove_employee.b3.getWidth()),
+                () -> assertEquals(30, remove_employee.b3.getHeight()),
+                () -> assertEquals(true, remove_employee.b3.getActionListeners())
+        );
+
+        assertAll("Name Label",
+                () -> assertEquals("Name:", remove_employee.l2.getText()),
+                () -> assertEquals(Color.white, remove_employee.l2.getForeground()),
+                () -> assertEquals(50, remove_employee.l2.getX()),
+                () -> assertEquals(150, remove_employee.l2.getY()),
+                () -> assertEquals(250, remove_employee.l2.getWidth()),
+                () -> assertEquals(30, remove_employee.l2.getHeight()),
+                () -> assertEquals("serif", remove_employee.l2.getFont().getName()),
+                () -> assertEquals(20, remove_employee.l2.getFont().getSize()),
+                () -> assertEquals(true, remove_employee.l2.getFont().isBold())
+        );
+
+        assertAll("Label6",
+                () -> assertEquals(Color.white, remove_employee.l6.getForeground()),
+                () -> assertEquals(200, remove_employee.l6.getX()),
+                () -> assertEquals(150, remove_employee.l6.getY()),
+                () -> assertEquals(350, remove_employee.l6.getWidth()),
+                () -> assertEquals(30, remove_employee.l6.getHeight()),
+                () -> assertEquals("serif", remove_employee.l6.getFont().getName()),
+                () -> assertEquals(20, remove_employee.l6.getFont().getSize()),
+                () -> assertEquals(true, remove_employee.l6.getFont().isBold())
+        );
+
+        assertAll("Mobile No Label",
+                () -> assertEquals("Mobile No:", remove_employee.l3.getText()),
+                () -> assertEquals(Color.white, remove_employee.l3.getForeground()),
+                () -> assertEquals(50, remove_employee.l3.getX()),
+                () -> assertEquals(200, remove_employee.l3.getY()),
+                () -> assertEquals(250, remove_employee.l3.getWidth()),
+                () -> assertEquals(30, remove_employee.l3.getHeight()),
+                () -> assertEquals("serif", remove_employee.l3.getFont().getName()),
+                () -> assertEquals(20, remove_employee.l3.getFont().getSize()),
+                () -> assertEquals(true, remove_employee.l3.getFont().isBold())
+        );
+
+        assertAll("Name Label",
+                () -> assertEquals(Color.white, remove_employee.l7.getForeground()),
+                () -> assertEquals(200, remove_employee.l7.getX()),
+                () -> assertEquals(200, remove_employee.l7.getY()),
+                () -> assertEquals(350, remove_employee.l7.getWidth()),
+                () -> assertEquals(30, remove_employee.l7.getHeight()),
+                () -> assertEquals("serif", remove_employee.l7.getFont().getName()),
+                () -> assertEquals(20, remove_employee.l7.getFont().getSize()),
+                () -> assertEquals(true, remove_employee.l7.getFont().isBold())
+        );
+
+        assertAll("Email Id Label",
+                () -> assertEquals("Email Id:", remove_employee.l4.getText()),
+                () -> assertEquals(Color.white, remove_employee.l4.getForeground()),
+                () -> assertEquals(50, remove_employee.l4.getX()),
+                () -> assertEquals(250, remove_employee.l4.getY()),
+                () -> assertEquals(250, remove_employee.l4.getWidth()),
+                () -> assertEquals(30, remove_employee.l4.getHeight()),
+                () -> assertEquals("serif", remove_employee.l4.getFont().getName()),
+                () -> assertEquals(20, remove_employee.l4.getFont().getSize()),
+                () -> assertEquals(true, remove_employee.l4.getFont().isBold())
+        );
+
+        assertAll("Label8",
+                () -> assertEquals(Color.white, remove_employee.l8.getForeground()),
+                () -> assertEquals(200, remove_employee.l8.getX()),
+                () -> assertEquals(250, remove_employee.l8.getY()),
+                () -> assertEquals(350, remove_employee.l8.getWidth()),
+                () -> assertEquals(30, remove_employee.l8.getHeight()),
+                () -> assertEquals("serif", remove_employee.l8.getFont().getName()),
+                () -> assertEquals(20, remove_employee.l8.getFont().getSize()),
+                () -> assertEquals(true, remove_employee.l8.getFont().isBold())
+        );
     }
 
     @After
